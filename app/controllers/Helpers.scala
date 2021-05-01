5116111cd4d93798b356ae435ac4d1f7285efd1f
MugishaU <muwiragiye123@gmail.com>
2021-05-01 22:23:07 +0100

    Add to helper method to allow query & string parameters to be passed in

--- app/controllers/Helpers.scala
+++ app/controllers/Helpers.scala
@@ -7,13 +7,14 @@ import play.api.mvc.Results.BadRequest
 case object Helpers {
   def handleRequestBody[T](
       request: Request[AnyContent],
-      response: T => Result
+      response: (T, List[String]) => Result,
+      params: List[String] = Nil
   )(implicit reads: Reads[T]): Result = {
     request.body.asJson
       .map { json =>
         json
           .validate[T]
-          .map(x => response(x))
+          .map(x => response(x, params))
           .recoverTotal { _ =>
             BadRequest(s"Malformed Request")
           }
