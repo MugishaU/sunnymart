b5ab1de27aa27025166064ad2400b5667938b53d
MugishaU <muwiragiye123@gmail.com>
2021-05-09 20:01:05 +0100

    Add JSON serialization for Customer Model

--- app/model/domain/Customer.scala
+++ app/model/domain/Customer.scala
@@ -16,21 +16,38 @@ final case class Address(
     postcode: String
 )
 
-final case class ExpiryDate(month: Int, year: Int)
-
-final case class PaymentInfo(
-    customerId: String,
-    cardNumber: String, //todo library called refined to create card number regex
-    expiryDate: ExpiryDate,
-    securityCode: Int
-)
-
 final case class Customer(
     id: String, //todo UUID?
     name: Name,
     address: Address
 )
 
+final case class ExpiryDate(month: Int, year: Int)
+
+final case class PaymentInfo(
+    id: String,
+    customerId: String,
+    cardNumber: String, //todo library called refined to create card number regex
+    expiryDate: ExpiryDate,
+    securityCode: Int
+)
+
+object Name {
+  implicit val nw: OWrites[Name] = Json.writes[Name]
+}
+
 object Address {
   implicit val ar: OWrites[Address] = Json.writes[Address]
+}
+
+object Customer {
+  implicit val cw: OWrites[Customer] = Json.writes[Customer]
+}
+
+object ExpiryDate {
+  implicit val edw: OWrites[ExpiryDate] = Json.writes[ExpiryDate]
+}
+
+object PaymentInfo {
+  implicit val piw: OWrites[PaymentInfo] = Json.writes[PaymentInfo]
 }
