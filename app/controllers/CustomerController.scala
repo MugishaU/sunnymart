b73aa6693c3623788ed8936085b1f676f5b20d53
MugishaU <muwiragiye123@gmail.com>
2021-05-09 19:58:23 +0100

    Add missing necessary endpoints for customer

--- app/controllers/CustomerController.scala
+++ app/controllers/CustomerController.scala
@@ -14,6 +14,13 @@ class CustomerController @Inject() (
 )(implicit ex: ExecutionContext)
     extends AbstractController(cc) {
 
+  def getCustomer(
+      customerId: String
+  ): Action[AnyContent] =
+    Action {
+      Ok(s"DELETE: customer-id: $customerId")
+    }
+
   def createCustomer: Action[AnyContent] =
     Action { request =>
       handleRequestBody[ApiCustomer](
@@ -22,15 +29,7 @@ class CustomerController @Inject() (
       )
     }
 
-  def createPaymentDetails(customerId: String): Action[AnyContent] =
-    Action { request =>
-      handleRequestBody[ApiPaymentInfo](
-        request,
-        (x, Nil) => Ok(s"Payment Details, customer-id: $customerId")
-      )
-    }
-
-  def editCustomer(customerId: String): Action[AnyContent] =
+  def updateCustomer(customerId: String): Action[AnyContent] =
     Action { request =>
       handleRequestBody[ApiCustomer](
         request,
@@ -43,5 +42,40 @@ class CustomerController @Inject() (
   ): Action[AnyContent] =
     Action {
       Ok(s"DELETE: customer-id: $customerId")
+    }
+
+  def getPaymentDetails(
+      customerId: String,
+      paymentId: String
+  ): Action[AnyContent] =
+    Action {
+      Ok(s"DELETE: customer-id: $customerId")
+    }
+
+  def createPaymentDetails(customerId: String): Action[AnyContent] =
+    Action { request =>
+      handleRequestBody[ApiPaymentInfo](
+        request,
+        (x, Nil) => Ok(s"Payment Details, customer-id: $customerId")
+      )
+    }
+
+  def updatePaymentDetails(
+      customerId: String,
+      paymentId: String
+  ): Action[AnyContent] =
+    Action { request =>
+      handleRequestBody[ApiPaymentInfo](
+        request,
+        (x, Nil) => Ok(s"Payment Details, customer-id: $customerId")
+      )
+    }
+
+  def deletePaymentDetails(
+      customerId: String,
+      paymentId: String
+  ): Action[AnyContent] =
+    Action { request =>
+      Ok("hi")
     }
 }
