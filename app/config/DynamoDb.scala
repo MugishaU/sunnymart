88f4e3d10a9660f11704b65569ffcc77b808c0c7
MugishaU <muwiragiye123@gmail.com>
2021-05-28 16:26:01 +0100

    Refactor getDynamoItem

--- app/config/DynamoDb.scala
+++ app/config/DynamoDb.scala
@@ -15,47 +15,56 @@ import scala.jdk.CollectionConverters._
 import scala.util.{Failure, Success, Try}
 
 object DynamoDb {
+  type DynamoDbResponse[T] = Either[Map[String, String], T]
+  type DynamoDbKeys = java.util.Map[String, AttributeValue]
+  type DynamoDbRequest = java.util.Map[String, AttributeValue]
   val dynamoDbClient: AmazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient
+
+  final case class PrimaryKey(name: String, value: String)
+  final case class SortKey(name: String, value: String)
 
   private def createError[T](
       errorMessage: String,
       statusCode: Int
-  ): Either[Map[String, String], T] = {
+  ): DynamoDbResponse[T] = {
     val error =
       Map("errorMessage" -> errorMessage, "statusCode" -> statusCode.toString)
     Left(error)
   }
 
-  def getItem[T](
-      primaryKeyName: String,
-      primaryKeyValue: String,
-      sortKeyName: Option[String] = None,
-      sortKeyValue: Option[String] = None,
-      tableName: String
-  )(implicit reads: Reads[T]): Either[Map[String, String], T] = {
-
-    val key = (sortKeyName, sortKeyValue) match {
-      case (Some(keyName), Some(keyValue)) =>
+  private def createDynamoKey(
+      primaryKey: PrimaryKey,
+      sortKey: Option[SortKey]
+  ): DynamoDbKeys = {
+    sortKey match {
+      case Some(sortKey) =>
         HashMap(
-          primaryKeyName -> new AttributeValue(primaryKeyValue),
-          keyName -> new AttributeValue(keyValue)
+          primaryKey.name -> new AttributeValue(primaryKey.value),
+          sortKey.name -> new AttributeValue(sortKey.value)
         ).asJava
-      case (_, _) =>
+      case None =>
         HashMap(
-          primaryKeyName -> new AttributeValue(primaryKeyValue)
+          primaryKey.name -> new AttributeValue(primaryKey.value)
         ).asJava
     }
+  }
 
+  private def sendDynamoDbRequest(
+      key: DynamoDbKeys,
+      tableName: String
+  ): Try[Option[DynamoDbRequest]] = {
     val request =
       new GetItemRequest()
         .withKey(key)
         .withTableName(tableName)
+    Try(Option(dynamoDbClient.getItem(request).getItem))
+  }
 
-    val item = Try(Option(dynamoDbClient.getItem(request).getItem))
-
-    item match {
+  private def convertDynamoDbResponse[T](
+      request: Try[Option[DynamoDbRequest]]
+  )(implicit reads: Reads[T]): DynamoDbResponse[T] = {
+    request match {
       case Failure(e: AmazonServiceException) =>
-        println(e)
         createError(e.getErrorMessage, e.getStatusCode)
 
       case Failure(_) =>
@@ -69,11 +78,22 @@ object DynamoDb {
         val awsItem = Try(Json.parse(jsonString).as[T])
 
         awsItem match {
-          case Failure(exception) =>
-            println(exception)
+          case Failure(_) =>
             createError("Failure to parse DynamoDB item", 500)
           case Success(awsItem) => Right(awsItem)
         }
     }
+  }
+
+  def getDynamoItem[T](
+      primaryKey: PrimaryKey,
+      sortKey: Option[SortKey] = None,
+      tableName: String
+  )(implicit reads: Reads[T]): DynamoDbResponse[T] = {
+
+    val key = createDynamoKey(primaryKey, sortKey)
+    val dynamoRequest = sendDynamoDbRequest(key, tableName)
+    convertDynamoDbResponse(dynamoRequest)
+
   }
 }
